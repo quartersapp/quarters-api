4f2ad7e0b1c9f00ad141e632cb4817273bc158cd
Thomas Dashney <tdashney@gmail.com>
2017-08-28 01:08:11 -0400

    refactor model by id dataloaders and fix user factory password issue

--- test-helpers/factories/user-factory.js
+++ test-helpers/factories/user-factory.js
@@ -1,11 +1,8 @@
-const { memoize } = require('lodash')
 const faker = require('faker')
-const passwordService = require('lib/auth/password-service')
+const { hashSync } = require('lib/auth/password-service')
 const createFactory = require('./create-factory')
-
-const hash = memoize(passwordService.hash)
 
 module.exports = createFactory({
   email: faker.internet.email,
-  passwordHash: () => hash('password')
+  passwordHash: () => hashSync(faker.internet.password(), 1)
 })
