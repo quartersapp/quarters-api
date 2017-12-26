efb3b4a98a0176b1829a64c391aa84321bbe7bd9
Thomas Dashney <tdashney@theworkinggroup.ca>
2017-12-26 02:32:37 -0500

    attempt to fix connection pool issues by limiting connections per request

--- bin/lambda.js
+++ bin/lambda.js
@@ -1,16 +1,4 @@
 const serverless = require('serverless-http')
-const connection = require('lib/db/connection')
 const app = require('lib/app')
 
-const invokeApp = serverless(app)
-
-/**
- * Connection pooling cannot be established reliably,
- * so we need to destroy the connection after each invocation
- */
-exports.handler = function (event, context, cb) {
-  invokeApp(event, context, (err, ...args) => {
-    connection.destroy()
-    cb(err, ...args)
-  })
-}
+exports.handler = serverless(app)
