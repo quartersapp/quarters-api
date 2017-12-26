8ff6bc32d0a9a797b405a6847798ad317d674543
Thomas Dashney <tdashney@theworkinggroup.ca>
2017-12-26 02:18:03 -0500

    update lambda func to destroy connection pool after each invocation

--- bin/lambda.js
+++ bin/lambda.js
@@ -1,4 +1,16 @@
 const serverless = require('serverless-http')
+const connection = require('lib/db/connection')
 const app = require('lib/app')
 
-exports.handler = serverless(app)
+const invokeApp = serverless(app)
+
+/**
+ * Connection pooling cannot be established reliably,
+ * so we need to destroy the connection after each invocation
+ */
+exports.handler = function (event, context, cb) {
+  invokeApp(event, context, (err, ...args) => {
+    connection.destroy()
+    cb(err, ...args)
+  })
+}
