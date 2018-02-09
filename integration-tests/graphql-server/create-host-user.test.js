9727d2fcc2802bd210136f02fce2cc7494a73645
Thomas Dashney <tdashney@theworkinggroup.ca>
2018-02-09 12:13:13 -0500

    signup mutation

---
+++ integration-tests/graphql-server/create-host-user.test.js
@@ -0,0 +1,39 @@
+/* eslint-env mocha */
+
+const request = require('supertest')
+const app = require('lib/app').listen()
+const { User } = require('lib/db/models')
+const { truncateModel, enableSnapshots } = require('test-helpers')
+
+describe('integration-tests/graphql-server/create-host-user', () => {
+  beforeEach(() => truncateModel(User))
+  beforeEach(enableSnapshots)
+
+  it('can create a new host user', async () => {
+    const variables = {
+      input: {
+        firstName: 'Lucas',
+        lastName: 'McLaughlin',
+        email: 'lucas.m@gmail.com',
+        password: 'pass1234'
+      }
+    }
+
+    const query = `
+      mutation CreateHostUser($input: CreateHostUserInput!) {
+        createHostUser(input: $input) {
+          user {
+            name,
+            email
+          }
+        }
+      }
+    `
+
+    const { body } = await request(app)
+      .post('/graphql')
+      .send({ query, variables })
+
+    expect(body).to.matchSnapshot()
+  })
+})
