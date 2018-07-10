5d0d7535500d068dc79c79cf44f64bd9f50968a5
Thomas Dashney <tdashney@theworkinggroup.ca>
2018-07-10 17:05:06 -0400

    use roles for hosts

--- db/migrations/20180705154511_hosts.js
+++ db/migrations/20180705154511_hosts.js
@@ -1,13 +1,12 @@
 exports.up = async (knex) => {
   // host user columns
   await knex.schema.alterTable('users', table => {
-    table.boolean('is_host').notNull().defaultTo(false)
     table.text('host_bio')
     table.string('host_google_place_id')
   })
 
   await knex.raw(`
-    ALTER TABLE users ADD CONSTRAINT host_required_params CHECK(NOT(is_host) OR (
+    ALTER TABLE users ADD CONSTRAINT host_required_params CHECK(NOT(roles \\? 'host') OR (
       host_bio IS NOT NULL AND
       host_google_place_id IS NOT NULL
     ))
@@ -16,10 +15,9 @@ exports.up = async (knex) => {
 
 exports.down = async (knex) => {
   await knex.raw(`
-    ALTER TABLE users REMOVE CONSTRAINT host_required_params
+    ALTER TABLE users DROP CONSTRAINT host_required_params
   `)
   await knex.schema.alterTable('users', table => {
-    table.dropColumn('is_host')
     table.dropColumn('host_bio')
     table.dropColumn('host_google_place_id')
   })
